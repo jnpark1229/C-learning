a9aaaf293f7393a6fba1c0a2a6fef47d439bbfb2
jnpark1229 <8060park12@gmail.com>
2025-02-01 23:00:34 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,62 @@
+# # print(abs(-5)) #5
+# # print(pow(4,2)) #4^2
+# # print(max(5,2)) #5
+# # print(min(5,12)) #5
+# # print(round(3.14)) #3
+
+# # from math import *
+# # print(floor(4.99)) #4
+# # print(ceil(3.14)) #4
+# # print(sqrt(16)) #4
+
+# # from random import*
+# # print(random())
+# # print(random()*10)
+# # print(int(random()*10))
+# # print(int(random()*10))
+# # print(int(random()*10))#0~10 미만의 임의의 값 생성
+
+# # print(int(random()*10)+1)#1~10 이하의 임의의 값 생성
+
+# # print(randrange(1,46))#1~46 미만의 임의의 값 생성
+
+# # print(randint(1,45))#1~45 이하의 임의의 값 생성
+
+# #quiz
+# # date=randint(4,28)
+# # print("오프라인 스터디 모임 날짜는 매월 "+str(date)+"일로 선정되었습니다",)
+
+
+# # sentence1="나는 소년이고 \n파이썬은 맛있어요"
+# # print(sentence1)
+# # sentence2="""
+# # 나는 소년이고 
+# # 파이썬은 맛있어요
+# # """
+# # print(sentence2)
+
+# # jumin="061229-3456789"
+# # print("성별:",jumin[7])
+# # print("연:",jumin[:2])  #0<=jumin<2
+# # print("월:",jumin[2:4])  #2<=jumin<4
+# # print("일:",jumin[4:6]) 
+
+# # print("생년월일:",jumin[:6])
+# # print("뒤 7자리",jumin[7:]) #7부터 끝까지
+# # print("뒤 7자리(뒤에서 부터)",jumin[-7:] ) # -7번째부터 끝까지
+
+# python="Python is Amazing"  
+# # print(python.lower())  
+# # print(python.upper())
+# # print(python[0].isupper())
+# # print(len(python))
+# # print(python.replace("Python","Java"))
+
+# # index=python.index("n")
+# # print(index)
+
+# print(python.find("Java")) #없어도 출력 씹지 않음
+# # print(python.index("Java")) #다음 출력 그냥 씹음
+# print("hi")
+
+# print(python.count("n"))#n이 몇 번 나왔는지 세 줌
